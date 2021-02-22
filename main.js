790c732486ce7e986172768f324ea8733399a5bd
Ronny <ronnykuruvilla@gmail.com>
2021-02-21 20:47:14 -0500

    Complete.

--- main.js
+++ main.js
@@ -2,9 +2,61 @@
  * YOUR CODE BELOW! *
  ********************/
 
+function moreThan5(num){
+  return num > 5;
+}
 
+function isNewTopScore(num, num2){
+  return num > num2;
+}
 
+function isInDanger(num){
+  return num >=60 && num<= 71;
+}
 
+function isCoasting(num){
+  return num >= 72 && num <= 83;
+  }
+
+function isSucceeding(num){
+  return num >= 84 && num <= 92;
+} 
+
+function isFailing(num){
+  return num < 60;
+}
+
+function isAcing(num){
+  return num > 92;
+}
+
+function isStudent(str){
+  return str === "student";
+}
+
+function isTeacher(str){
+  return str === "teacher";
+}
+
+function isAdmin(str){
+  return str === "admin";
+}
+
+function isElementary(level){
+  return level === "elementary";
+}
+
+function areDifferentPeople(person1, person2){
+  return person1 !== person2;
+}
+
+function isMiddleSchoolTeacher(role, grade){
+  return role === "teacher" && grade >= 6 && grade <= 8;
+}
+
+function notAnElementarySchoolAdministrator(level, role){
+  return level !== "elementary" || role !== "admin";
+}
 
 /********************************************************************************************
  * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
